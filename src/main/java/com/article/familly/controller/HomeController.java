cbd43c3b26edf969de917e58cd812906f6e36862
jeehk1108 <50824836+jeehk1108@users.noreply.github.com>
2020-12-18 16:46:08 +0900

    [JEE 2020-12-18] 작업추가

---
+++ src/main/java/com/article/familly/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.article.familly.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+
+}
